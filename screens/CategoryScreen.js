61c328c7a288f791f92930b060e7bb8b0c6f0965
rodakd <dawidrodak@yahoo.com>
2020-04-05 21:06:26 +0200

    Add things to modal

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,13 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { View, FlatList, Platform } from 'react-native';
+import React, { useEffect, useState, useRef } from 'react';
+import { View, FlatList, Platform, Text, StyleSheet, Dimensions, Button } from 'react-native';
+import { Input } from 'react-native-elements';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import Modal from 'react-native-modal';
+import { TextInput } from 'react-native-gesture-handler';
 import RenderCategory from '../components/RenderCategory';
 import * as recipeActions from '../store/recipeActions';
 import IoniconsHeaderButton from '../components/IoniconsHeaderButton';
 
 const CategoryScreen = ({ navigation }) => {
-  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const categories = [];
+  const input = useRef(null);
 
   useEffect(() => {
     recipeActions.getRecipes();
@@ -18,7 +22,7 @@ const CategoryScreen = ({ navigation }) => {
             title="add"
             iconName={Platform.OS === 'ios' ? 'ios-add-circle' : 'md-add-circle'}
             onPress={() => {
-              setIsOverlayVisible(true);
+              setIsModalVisible(true);
             }}
           />
         </HeaderButtons>
@@ -28,6 +32,18 @@ const CategoryScreen = ({ navigation }) => {
 
   return (
     <View>
+      <Modal
+        style={styles.modal}
+        isVisible={isModalVisible}
+        useNativeDriver
+        onBackdropPress={() => setIsModalVisible(false)}
+      >
+        <View style={styles.card}>
+          <Text style={styles.cardHeader}> New category </Text>
+          <Input ref={input} label="Category name" containerStyle={{ margin: 10 }} />
+          <Button title="Add category" onPress={() => console.log(input.current.isFocused())} />
+        </View>
+      </Modal>
       <FlatList
         numColumns={2}
         renderItem={({ item }) => <RenderCategory item={item} />}
@@ -37,6 +53,32 @@ const CategoryScreen = ({ navigation }) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4.65,
+    elevation: 8,
+    width: Dimensions.get('window').width / 1.3,
+    height: Dimensions.get('window').height / 2,
+    borderRadius: 20,
+    alignItems: 'center',
+    padding: 10,
+  },
+  cardHeader: {
+    fontSize: 24,
+  },
+});
 
 export const categoryScreenOptions = (navData) => {
   return {
