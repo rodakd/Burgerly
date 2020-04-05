8021535774278bb6e9ab4bc158d57ef8ddae5afb
rodakd <dawidrodak@yahoo.com>
2020-04-05 18:50:01 +0200

    Add modal

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,34 +1,47 @@
-import React, { useEffect } from 'react';
-import { FlatList, Platform } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, FlatList, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import RenderCategory from '../components/RenderCategory';
 import * as recipeActions from '../store/recipeActions';
 import IoniconsHeaderButton from '../components/IoniconsHeaderButton';
 
-const CategoryScreen = () => {
+const CategoryScreen = ({ navigation }) => {
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
   const categories = [];
 
   useEffect(() => {
     recipeActions.getRecipes();
+    navigation.setOptions({
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+          <Item
+            title="add"
+            iconName={Platform.OS === 'ios' ? 'ios-add-circle' : 'md-add-circle'}
+            onPress={() => {
+              setIsOverlayVisible(true);
+            }}
+          />
+        </HeaderButtons>
+      ),
+    });
   });
 
   return (
-    <FlatList
-      numColumns={2}
-      renderItem={({ item }) => <RenderCategory item={item} />}
-      data={categories}
-      keyExtractor={(item) => item.id.toString()}
-    />
+    <View>
+      <FlatList
+        numColumns={2}
+        renderItem={({ item }) => <RenderCategory item={item} />}
+        data={categories}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
   );
 };
 
-export const categoryScreenOptions = {
-  headerTitle: 'Categories',
-  headerRight: () => (
-    <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
-      <Item title="add" iconName={Platform.OS === 'ios' ? 'ios-add-circle' : 'md-add-circle'} />
-    </HeaderButtons>
-  ),
+export const categoryScreenOptions = (navData) => {
+  return {
+    headerTitle: 'Categories',
+  };
 };
 
 export default CategoryScreen;
