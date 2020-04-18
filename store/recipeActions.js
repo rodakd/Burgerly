8c7303739397effc2bf873645950d0294bc2a193
rodakd <dawidrodak@yahoo.com>
2020-04-18 16:53:10 +0200

    Add recipe actions

--- store/recipeActions.js
+++ store/recipeActions.js
@@ -1,10 +1,26 @@
 import { Alert } from 'react-native';
-import { insertCategory, fetchCategories, deleteCategory, updateCategory } from '../helper/db';
+import {
+  insertCategory,
+  insertRecipe,
+  fetchCategories,
+  fetchRecipes,
+  deleteCategory,
+  deleteRecipe,
+  updateCategory,
+  updateRecipe,
+} from '../helper/db';
 
 export const SET_CATEGORIES = 'SET_CATEGORIES';
+export const SET_RECIPES = 'SET_RECIPES';
+
 export const ADD_CATEGORY = 'ADD_CATEGORY';
+export const ADD_RECIPE = 'ADD_RECIPE';
+
 export const DELETE_CATEGORY = 'DELETE_CATEGORY';
+export const DELETE_RECIPE = 'DELETE_RECIPE';
+
 export const EDIT_CATEGORY = 'EDIT_CATEGORY';
+export const EDIT_RECIPE = 'EDIT_RECIPE';
 
 export const setCategories = () => {
   return async (dispatch) => {
@@ -21,25 +37,79 @@ export const setCategories = () => {
   };
 };
 
-export const addCategory = (title, color) => {
+export const setRecipes = () => {
   return async (dispatch) => {
-    insertCategory({ title, color })
+    fetchRecipes()
+      .then((dbResult) =>
+        dispatch({
+          type: SET_RECIPES,
+          payload: dbResult.rows._array,
+        })
+      )
+      .catch((err) => {
+        Alert.alert(err, 'Please try again', { text: 'Okay' });
+      });
+  };
+};
+
+export const addCategory = (title, color) => {
+  const category = { title, color };
+
+  return async (dispatch) => {
+    insertCategory(category)
       .then()
       .catch((err) => {
         Alert.alert(err, 'Please try again', [{ text: 'Okay' }]);
       });
-    dispatch({ type: ADD_CATEGORY, payload: { title, color } });
+    dispatch({ type: ADD_CATEGORY, payload: category });
+  };
+};
+
+export const addRecipe = (title, image, duration, difficulty, calories, ingredients, steps) => {
+  const recipe = { title, image, duration, difficulty, calories, ingredients, steps };
+
+  return async (dispatch) => {
+    insertRecipe(recipe)
+      .then()
+      .catch((err) => {
+        Alert.alert(err, 'Please try again', [{ text: 'Okay' }]);
+      });
+    dispatch({ type: ADD_RECIPE, payload: recipe });
   };
 };
 
 export const editCategory = (id, title, color) => {
+  const category = { id, title, color };
+
   return async (dispatch) => {
-    updateCategory({ id, title, color })
+    updateCategory(category)
       .then()
       .catch((err) => {
         Alert.alert(err, 'Please try again', { text: 'Okay' });
       });
-    dispatch({ type: EDIT_CATEGORY, payload: { id, title, color } });
+    dispatch({ type: EDIT_CATEGORY, payload: category });
+  };
+};
+
+export const editRecipe = (
+  id,
+  title,
+  image,
+  duration,
+  difficulty,
+  calories,
+  ingredients,
+  steps
+) => {
+  const recipe = { id, title, image, duration, difficulty, calories, ingredients, steps };
+
+  return async (dispatch) => {
+    updateRecipe(recipe)
+      .then()
+      .catch((err) => {
+        Alert.alert(err, 'Please try again', { text: 'Okay' });
+      });
+    dispatch({ type: EDIT_RECIPE, payload: recipe });
   };
 };
 
@@ -54,4 +124,13 @@ export const trashCategory = (id) => {
   };
 };
 
-export const addRecipe = () => {};
+export const trashRecipe = (id) => {
+  return async (dispatch) => {
+    deleteRecipe(id)
+      .then()
+      .catch((err) => {
+        Alert.alert(err, 'Please try again', { text: 'Okay' });
+      });
+    dispatch({ type: DELETE_RECIPE, payload: id });
+  };
+};
