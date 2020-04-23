9c9ee7a412d7634cf933ddc1526a99013e2e7547
rodakd <dawidrodak@yahoo.com>
2020-04-23 16:39:43 +0200

    Add recipe details screen

--- screens/RecipeDetailsScreen.js
+++ screens/RecipeDetailsScreen.js
@@ -1,19 +1,55 @@
+// TODO Fix header for long names
+
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
+import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useDispatch } from 'react-redux';
+import { IoniconsHeaderButton } from '../components';
 import Colors from '../constants/Colors';
+import { trashRecipe } from '../store/recipeActions';
 
 const RecipeDetailsScreen = (props) => {
   const { navigation, route } = props;
 
+  const dispatch = useDispatch();
+  const { recipe } = route.params;
+  const { title, image, difficulty, duration, calories } = recipe;
+  const ingredients = JSON.parse(recipe.ingredients);
+  const steps = JSON.parse(recipe.steps);
+
   useEffect(() => {
     navigation.setOptions({
-      headerTitle: route.params.recipe.title,
+      headerTitle: recipe.title,
+      headerRight: () => {
+        return (
+          <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+            <Item
+              title="edit"
+              iconName="md-create"
+              onPress={() => {
+                navigation.navigate('edit', { recipe: { ...recipe, ...{ ingredients, steps } } });
+              }}
+            />
+            <Item
+              title="trash"
+              iconName="md-trash"
+              onPress={() => {
+                dispatch(trashRecipe(recipe.id));
+                navigation.goBack();
+              }}
+            />
+          </HeaderButtons>
+        );
+      },
     });
   }, [navigation]);
 
   return (
-    <View style={styles.container}>
-      <Text>Sup nigga</Text>
+    <View style={{ ...styles.container, ...{ padding: wp(5) } }}>
+      {recipe.image && (
+        <Image style={{ ...styles.image, ...{ borderRadius: wp(5) } }} source={{ uri: image }} />
+      )}
     </View>
   );
 };
@@ -21,7 +57,12 @@ const RecipeDetailsScreen = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
     backgroundColor: Colors.background,
+  },
+  image: {
+    width: '100%',
+    height: '40%',
   },
 });
 
