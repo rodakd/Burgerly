a7af994778b40b763d228cb01b45558a0460387c
rodakd <dawidrodak@yahoo.com>
2020-05-02 19:00:16 +0200

    Remove DraggableListScreen

---
+++ screens/StepsScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
+const StepsScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+StepsScreen.propTypes = {};
+StepsScreen.defaultProps = {};
+
+export default StepsScreen;
